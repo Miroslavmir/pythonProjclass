bf21f96f87fc60f13aa02da28422b11ea4ec9680
gk <gk@adamant.local>
2023-06-12 21:11:22 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,49 @@
+
+# somelist = [37, 0, 50, 46, 34, 46, 0, 13]
+# index = int(input('vvod'))
+# if index > len(somelist):
+#     print('index')
+# else:
+#     print(somelist[index])
+# print(somelist[::-1])
+#
+# somelist = [37, 0, 50, 46, 34, 46, 0, 13]
+#
+# index = 0
+# while index < len(somelist):
+#     somelist[index] *= 2
+#     index += 1
+#
+# print(somelist)
+
+# # 11.17 ve
+# somelist = [37, 0, 50, 46, 34, 46, 0, 13]
+# index = 0
+# firstnumber = somelist[0]
+#
+# while index < len(somelist):
+#     somelist[index] /= firstnumber
+#     index += 1
+# print(somelist)
+
+#11.19 a
+# somelist = [37, 0, 50, 46, 34, 46, 0, 13]
+# sumnumber = sum(somelist)
+# print(sumnumber)
+#srednee arifmetik
+# somelist = [37, 0, 50, 46, 34, 46, 0, 13]
+# sumnumber = sum (somelist) / len(somelist)
+# print(sumnumber)
+
+# somelist = [37, 0, 50, 46, 34, 46, 0, 13]
+# minnumber = min(somelist)
+# maxnumber = max(somelist)
+# result = maxnumber - minnumber
+# print(result)
+
+# somelist = ['Hello', 'World']
+# index = 0
+# someworld = somelist[0]
+# print(someworld[0]) # [1],[2],[3], [4], [5]
+
+# 11.16 абв, 11.18, 11.21, 11.30. 11.36 аб 11.39,11.52 абв.11.53 абв
